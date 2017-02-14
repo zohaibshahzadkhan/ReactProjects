f81b53742b50ed8123a4747e8888938708733c26
zohaibshahzadkhan <Zohaibshahzad16@gmail.com>
2017-02-14 11:40:19 +0500

    app structure completed

---
+++ TimerApplication/src/app.jsx
@@ -0,0 +1,56 @@
+class TimerWrapper extends React.Component
+{
+    constructor(props)
+    {
+
+    }
+
+    StartTimer(timeLeft)
+    {
+
+    }
+
+    render()
+    {
+
+    }
+}
+
+class Timer extends React.Component
+{
+    render()
+    {
+
+    }
+}
+
+
+class Button extends React.Component
+{
+    StartTimer(event)
+    {
+
+    }
+    render()
+    {
+
+    }
+}
+
+
+ReactDOM.render(
+<TimerWrapper/>,
+document.getElementById('timer')
+)
+
+
+
+
+
+
+
+
+
+
+
+
